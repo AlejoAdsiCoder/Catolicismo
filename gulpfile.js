1ea2810633e3e2da5291a28d8e784764ade0116b
Alejok <Alejok>
2019-10-16 00:08:21 -0500

    section noticias

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
-var gulp = require('gulp');
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync'),
+  autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('hello', function() {
-    console.log('Hello');
+
+function style() {
+  return gulp.src('./src/sass/arquidiocesis.style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
   });
+  gulp.watch('./src/sass/**/*.scss', style);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./src/js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
